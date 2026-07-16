96dfbb1791e3c570c4444ada219c3408b5114aae
hameedshk <kshameed7@gmail.com>
2026-07-16 18:55:57 +0530

    gitignore

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_owlen_app_OwlenApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.owlen.app.OwlenApplication"
-)
-public final class _com_owlen_app_OwlenApplication {
-}
